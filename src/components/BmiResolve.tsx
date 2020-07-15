bcadc28256aaa2652d2ef625fa13fd9521adaade
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-15 15:58:44 -0400

    styling for ionioc

--- src/components/BmiResolve.tsx
+++ src/components/BmiResolve.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { IonRow, IonCol, IonCard, IonCardContent } from '@ionic/react';
 
+import './BmiResolve.css';
+
 const BmiResolve: React.FC<{result: number }> = props => {
 
     return (
         <IonRow>
             <IonCol>
-              <IonCard>
+              <IonCard color="secondary" >
                 <IonCardContent>
                     <h2 className="ion-text-center">Your body-mass-index</h2>
                   <h3 className="ion-text-center">{props.result.toFixed(2)}</h3>
